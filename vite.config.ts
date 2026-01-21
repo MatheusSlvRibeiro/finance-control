3422d194167f3128badb4791011878c4a2eac7dc
Matheus da Silva <silva.contadev@gmail.com>
2026-01-21 15:41:29 -0300

    :sparkles: feat(alias-path): add /types path alias

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 			"@pages": path.resolve(__dirname, "src/pages"),
 			"@styles": path.resolve(__dirname, "src/styles"),
 			"@utils": path.resolve(__dirname, "src/utils"),
+			"@types": path.resolve(__dirname, "src/types/"),
 		},
 	},
 	css: {
